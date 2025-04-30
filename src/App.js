062acb7d9a390f599e8a2436cbd397c4a0d8b5fc
Kingsoman <konyeagusi@gmail.com>
2025-05-01 03:24:16 +0500

    fix styling, and updated google sheet id

--- src/App.js
+++ src/App.js
@@ -9,12 +9,12 @@ function App() {
   const ref = useRef();
   const [idx, setIdx] = useState(0);
   const [userData, setUserData] = useState([]);
-  const [currentData, setCurrentData] = useState();
+  const [currentData, setCurrentData] = useState(null);
 
-  let shetId = "1NUI1nevsjrE0SbyoSOoG_bFWpoRiFGTxu732w2QFDRs";
+  let shetId = "1_m1r4uqhlNMosyy5GQBg0XgqqKGCqfJWyydwnIARAxg";
   let shetTitle =
-    "reportSubmissions_Wed Jan 11 2023 13:10:52 GMT+0100 (heure normale d’Europe centrale)";
-  let shetCount = "A2:B500";
+    "reportSubmissions_Wed Jan 11 2023 13:10:52 GMT+0100 (heure normale d'Europe centrale)";
+  let shetCount = "A2:C100";
 
   let fullUrl =
     "https://docs.google.com/spreadsheets/d/" +
@@ -46,19 +46,14 @@ function App() {
     }
   };
 
-  const handleGenerator = async () => {
-    setIdx((prevTime) => prevTime + 1);
-    setCurrentData(userData[idx + 1]);
-    const element = ref.current;
+  const downloadImage = async (element) => {
     const canvas = await html2canvas(element, { useCORS: true, scale: 2 });
-
     const dataset = canvas.toDataURL("image/png");
     const link = document.createElement("a");
 
     if (typeof link.download === "string") {
       link.href = dataset;
       link.download = "image.png";
-
       document.body.appendChild(link);
       link.click();
       document.body.removeChild(link);
@@ -67,42 +62,38 @@ function App() {
     }
   };
 
+  const handleGenerator = async () => {
+    setIdx((prevTime) => prevTime + 1);
+    setCurrentData(userData[idx + 1]);
+    await downloadImage(ref.current);
+  };
+
   const handleScript = async () => {
-    let id = idx
-    let abc = setInterval(async () => {
-      id = id + 1
+    let id = idx;
+    const interval = setInterval(async () => {
+      id = id + 1;
       setIdx(id);
       setCurrentData(userData[id]);
-      const element = ref.current;
-      const canvas = await html2canvas(element, { useCORS: true, scale: 2 });
-
-      const dataset = canvas.toDataURL("image/png");
-      const link = document.createElement("a");
-
-      if (typeof link.download === "string") {
-        link.href = dataset;
-        link.download = "image.png";
-
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-      } else {
-        window.open(dataset);
-      }
+      await downloadImage(ref.current);
     }, 1000);
+
     setTimeout(() => {
-      clearInterval(abc);
+      clearInterval(interval);
     }, 10000);
   };
 
   return (
     <div>
-      <Header />
       {!!userData.length ? (
         <div className="App">
           <div className="card-box" ref={ref}>
-            <img src={card} className="card" />
-            <img src={currentData?.c?.[1]?.v} className="user-img" />
+            <img src={card} className="card" alt="Card template" />
+            <img 
+              src={currentData?.c?.[2]?.v} 
+              className="user-img" 
+              alt="User profile" 
+              style={{border: '1px solid gainsboro'}} 
+            />
             <p className="user-name">@{currentData?.c?.[0]?.v}</p>
           </div>
           <div className="butRow">
@@ -110,21 +101,21 @@ function App() {
               Back
             </Button>
             <div>
-            <Button
-              className="button"
-              variant="contained"
-              onClick={handleGenerator}
-            >
-              Download Card
-            </Button>
-            <Button
-              className="button"
-              variant="contained"
-              onClick={handleScript}
-            >
-              Print 10 Cards
-            </Button>
-          </div>
+              <Button
+                className="button"
+                variant="contained"
+                onClick={handleGenerator}
+              >
+                Download
+              </Button>
+              <Button
+                className="button"
+                variant="contained"
+                onClick={handleScript}
+              >
+                Print 10 Cards
+              </Button>
+            </div>
             <Button className="btn" variant="contained" onClick={handleNext}>
               Next
             </Button>
